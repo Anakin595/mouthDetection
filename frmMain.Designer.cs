21427fe0ad1bb6ae3365cfd17b206ffa516e3495
Anakin595 <gizynski.krystian@gmail.com>
2017-02-22 23:54:55 +0100

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace PIW_Mouth_detection
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tlpOuter = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpInner = new System.Windows.Forms.TableLayoutPanel();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.lblChosenFile = new System.Windows.Forms.Label();
+            this.ibOriginal = new Emgu.CV.UI.ImageBox();
+            this.ibCanny = new Emgu.CV.UI.ImageBox();
+            this.ofdOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.tlpOuter.SuspendLayout();
+            this.tlpInner.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibCanny)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlpOuter
+            // 
+            this.tlpOuter.ColumnCount = 2;
+            this.tlpOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpOuter.Controls.Add(this.tlpInner, 0, 0);
+            this.tlpOuter.Controls.Add(this.ibOriginal, 0, 1);
+            this.tlpOuter.Controls.Add(this.ibCanny, 1, 1);
+            this.tlpOuter.Controls.Add(this.imageBox1, 0, 2);
+            this.tlpOuter.Controls.Add(this.imageBox2, 1, 2);
+            this.tlpOuter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpOuter.Location = new System.Drawing.Point(0, 0);
+            this.tlpOuter.Name = "tlpOuter";
+            this.tlpOuter.RowCount = 3;
+            this.tlpOuter.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpOuter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpOuter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpOuter.Size = new System.Drawing.Size(868, 461);
+            this.tlpOuter.TabIndex = 0;
+            this.tlpOuter.Paint += new System.Windows.Forms.PaintEventHandler(this.tlpOuter_Paint);
+            // 
+            // tlpInner
+            // 
+            this.tlpInner.AutoSize = true;
+            this.tlpInner.ColumnCount = 2;
+            this.tlpOuter.SetColumnSpan(this.tlpInner, 2);
+            this.tlpInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlpInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpInner.Controls.Add(this.btnOpenFile, 0, 0);
+            this.tlpInner.Controls.Add(this.lblChosenFile, 1, 0);
+            this.tlpInner.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpInner.Location = new System.Drawing.Point(3, 3);
+            this.tlpInner.Name = "tlpInner";
+            this.tlpInner.RowCount = 1;
+            this.tlpInner.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpInner.Size = new System.Drawing.Size(862, 36);
+            this.tlpInner.TabIndex = 0;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.AutoSize = true;
+            this.btnOpenFile.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnOpenFile.Location = new System.Drawing.Point(3, 3);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(91, 30);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open file";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // lblChosenFile
+            // 
+            this.lblChosenFile.AutoSize = true;
+            this.lblChosenFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblChosenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblChosenFile.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblChosenFile.Location = new System.Drawing.Point(100, 0);
+            this.lblChosenFile.Name = "lblChosenFile";
+            this.lblChosenFile.Size = new System.Drawing.Size(759, 36);
+            this.lblChosenFile.TabIndex = 1;
+            this.lblChosenFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ibOriginal
+            // 
+            this.ibOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ibOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibOriginal.Enabled = false;
+            this.ibOriginal.Location = new System.Drawing.Point(3, 45);
+            this.ibOriginal.Name = "ibOriginal";
+            this.ibOriginal.Size = new System.Drawing.Size(428, 203);
+            this.ibOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ibOriginal.TabIndex = 2;
+            this.ibOriginal.TabStop = false;
+            // 
+            // ibCanny
+            // 
+            this.ibCanny.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ibCanny.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibCanny.Enabled = false;
+            this.ibCanny.Location = new System.Drawing.Point(437, 45);
+            this.ibCanny.Name = "ibCanny";
+            this.ibCanny.Size = new System.Drawing.Size(428, 203);
+            this.ibCanny.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ibCanny.TabIndex = 2;
+            this.ibCanny.TabStop = false;
+            // 
+            // ofdOpenFile
+            // 
+            this.ofdOpenFile.FileName = "openFileDialog1";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Enabled = false;
+            this.imageBox1.Location = new System.Drawing.Point(3, 254);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(428, 204);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox2.Enabled = false;
+            this.imageBox2.Location = new System.Drawing.Point(437, 254);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(428, 204);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 461);
+            this.Controls.Add(this.tlpOuter);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tlpOuter.ResumeLayout(false);
+            this.tlpOuter.PerformLayout();
+            this.tlpInner.ResumeLayout(false);
+            this.tlpInner.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibCanny)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpOuter;
+        private System.Windows.Forms.TableLayoutPanel tlpInner;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label lblChosenFile;
+        private Emgu.CV.UI.ImageBox ibOriginal;
+        private Emgu.CV.UI.ImageBox ibCanny;
+        private System.Windows.Forms.OpenFileDialog ofdOpenFile;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;                  //
+using Emgu.CV.CvEnum;           // usual Emgu Cv imports
+using Emgu.CV.Structure;        //
+using Emgu.CV.UI;               //
+using Emgu.CV.Util;
+using System.Text.RegularExpressions;
+
+namespace PIW_Mouth_detection
+{
+    
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tlpOuter_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            DialogResult drChosenFile;
+
+            
+            // otwarcie okna dialogowego do wyboru pliku
+            drChosenFile = ofdOpenFile.ShowDialog();
+
+            if (drChosenFile != DialogResult.OK || ofdOpenFile.FileName == "")
+            {
+                lblChosenFile.Text = "file not chosen";
+                return;
+            }
+
+            Mat imgOriginal;
+
+            try
+            {
+                imgOriginal = new Mat(ofdOpenFile.FileName);
+            }
+            catch (Exception ex)
+            {
+                lblChosenFile.Text = "unable to open image, error: " + ex.Message;
+                return;
+            }
+
+            if (imgOriginal == null)
+            {
+                lblChosenFile.Text = "unable to open image";
+                return;
+            }
+            
+            // Mat imgRed = new Mat(imgOriginal.Size, DepthType.Cv8U, 1);
+            Image<Bgr, byte> img = imgOriginal.ToImage<Bgr,byte>();
+            Image<Ycc, Byte> Yimg = img.Convert<Ycc, Byte>();
+            Image<Ycc, Byte> imgGray = img.Convert<Ycc, Byte>();
+            Image<Gray, Byte> imgY= Yimg[0].Convert<Gray, Byte>(); // Ycc -->    [0] - Luminance, [1] - Cr, [2] - Cb, 
+            Image<Gray, Byte> imgCb = Yimg[2].Convert<Gray, Byte>();
+            Image<Gray, Byte> imgCr = Yimg[1].Convert<Gray, Byte>();
+            Image<Gray, Byte> imgCr2 = imgCr;
+
+            Image<Gray, byte> tempImg = imgY.Convert<Gray,byte>();
+            
+            imgGray._EqualizeHist();
+            
+            // Pixel:    0 = black, 255 = white
+
+            Byte[,,] data = Yimg.Data;
+            Byte[,,] dataCr = imgCr.Data;
+            Byte[,,] dataCr2 = imgCr.Data;
+            int rows = img.Rows;
+            int cols = img.Cols;
+            Byte [,,] data1 = new Byte [rows, cols,1];
+            double Cr = 0, Cr2 = 0;
+
+            // wyznaczanie maksymalnej wartości piksela kanału Cr, w celu zlokalizowania ust
+            double maxCr = max_pix(dataCr, rows, cols);
+
+            // pętla tworząca obraz końcowy zawierający usta w kolorze białym
+            for (int i =0; i<rows; i++)
+            {
+                for (int j = 0; j<cols; j++)
+                {
+                    Cr = dataCr[i, j, 0];      // kanał Cr z YCbCr
+                    Cr2 = Math.Floor(Math.Pow(Cr, 2)*255/(maxCr*maxCr));
+                    dataCr2[i, j, 0] = (byte)(Cr2*Cr2/255);
+                    if (dataCr[i, j, 0] > 190) // próg odczytu czerwieni
+                        data1[i, j, 0] = 255;
+                    else
+                        data1[i, j, 0] = 0;               
+                }  
+            }
+
+            // rysowanie prostokąta wokół ust
+            int a = (int)(0.05*rows), b = (int)(0.03*cols);
+            int[] srodek = weightPoint(data1, rows, cols);
+            Point p1 = new Point { X = srodek[0]+a, Y = srodek[1]-b };
+            Point p2 = new Point { X = srodek[0]+a, Y = srodek[1]+b };
+            Point p3 = new Point { X = srodek[0]-a, Y = srodek[1]+b };
+            Point p4 = new Point { X = srodek[0]-a, Y = srodek[1]-b };
+
+            Point[] pts =new Point[] {p1,p2,p3,p4};
+            img.DrawPolyline(pts,true,new Bgr(153,255,51),3);
+
+            imgCr2.Data = dataCr2;
+            tempImg.Data = data1;
+
+            CvInvoke.GaussianBlur(tempImg, tempImg, new Size(5, 5), 1.5);
+
+
+            // Wyświetlenie obraców, kolejno: 
+            //  Oryginał, Orginał z oznaczonymi ustami, 
+            //  kanał Cr podniesiony do kwadratu i znormalizowany do [0,255],
+            //  wynik klasyfikacji obrazu kanału Cr^2  określoną granicą
+            ibOriginal.Image = imgOriginal;
+            ibCanny.Image = img; 
+            imageBox1.Image = imgCr2;
+            imageBox2.Image =tempImg;
+        }
+
+
+        // obliczanie maksymalnej i minimalnej wartości poksela obrazu
+        public double max_pix(Byte[,,] d, int row, int col)
+        {
+            double max = 0;
+            for (int i = 0; i < row-2; i++)
+            {
+                for (int j = 0; j < col-2; j++)
+                {
+                    if (d[i, j, 0] > max)
+                        max = d[i, j, 0];
+                }
+            }
+            return max;
+        }
+        public double min_pix(Byte[,,] d, int row, int col)
+        {
+            double min = 255;
+            for (int i = 0; i < row - 2; i++)
+            {
+                for (int j = 0; j < col - 2; j++)
+                {
+                    if (d[i, j, 0] < min)
+                        min = d[i, j, 0];
+                }
+            }
+            return min;
+        }
+
+        // środek ciężkości obrazu czarno-białego
+        public int[] weightPoint(Byte[,,] d, int row, int col)
+        {
+            int[] weights = { 0, 0 };
+            int count = 0;
+            for (int i = 0; i < row - 2; i++)
+            {
+                for (int j = 0; j < col - 2; j++)
+                {
+                    if (d[i, j, 0] != 0)
+                    {
+                        weights[1] += i;
+                        weights[0] += j;
+                        count++;
+                    }                       
+                }
+            }
+            weights[0] /= count;
+            weights[1] /= count;
+
+            return weights;
+        }
+    }
+}
